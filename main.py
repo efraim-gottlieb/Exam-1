458c9a6b997331c1f9189577a210e07843ffa156
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-23 12:42:12 +0300

    add gui

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from game_logic.game import play_round
 def main():
     game = init_game()
     while game['player_1']['hand'] and game['player_2']['hand']:
+        input('Press Enter to play a raound ')
         play_round(game['player_1'], game['player_2'])
 
     winner = max(len(game['player_1']['won_pile']), len(game['player_2']['won_pile']))
@@ -12,10 +13,12 @@ def main():
     elif len(game['player_1']['won_pile']) < len(game['player_2']['won_pile']):
         winner = game["player_2"]['name']
 
-    print(f'{'=' * 30}\n{' ' * 10}Game Over')
+    print(f'{'=' * 30}\n{' ' * 10}Game Over\n{' ' * 9}{'-' * 11}')
     if len(game['player_1']['won_pile']) == len(game['player_2']['won_pile']):
-        print('DRAW')
-    else:
-        print(f'{winner} win the game')
 
-main()
+        print(f'\n{' ' * 11}DRAW !')
+    else:
+        print(f'\n{' ' * 6}{winner} win the game !')
+
+if __name__ == '__main__':
+    main()
