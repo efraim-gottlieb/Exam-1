243773e869a0fed37a7c9fd4c26d8d23780ca1e5
EfraimGottlieb <efgo583208979@gmail.com>
2025-10-23 09:58:28 +0300

    added game dirs and files

---
+++ utils/deck.py
@@ -0,0 +1,11 @@
+def create_card(rank:str,suite:str):
+    return {}
+
+def compare_cards(p1_card:dict, p2_card:dict):
+    return ""
+
+def create_deck():
+    return list[dict]
+
+def shuffle(deck:list[dict]):
+    return list[dict]
